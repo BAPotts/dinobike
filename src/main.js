53a7707a4d95e3fa32a75192e66b3aa201b56be7
Beverly Potts <pottsbeverly@gmail.com>
2020-07-02 18:01:47 -0400

    Add failing code for handling and siplaying dinosaur words

--- src/main.js
+++ src/main.js
@@ -2,7 +2,18 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+import { getDino } from './src/dinosaur-service';
+
+async function getDinoWords() {
+  const apiResponse = await getDino();
+  if(!apiResponse) {
+    $(".display").html('<p>Something Went Wrong!</p>');
+  } else {
+    $('.display').text(apiResponse);
+  }
+}
+    
 
 $(document).ready(function() {
-
+  getDinoWords();
 });
